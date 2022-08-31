74761270f26255249a8c114b58b8506592ae556c
personA <personA@iru.university>
2022-08-31 17:07:40 +0300

    Connect To Database

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const uri: string = process.env.DATABASE_URI ?? "";
+
+mongoose.connect(uri).catch((err: any) => console.log(err));
+
+const connection = mongoose.connection;
+connection.once("open", async () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
